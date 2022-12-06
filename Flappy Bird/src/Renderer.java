6f5f23aa362e8460991c18216aed4af21cb40b62
Rasmiranjan Nayak <rasmiranjan701@gmail.com>
2022-12-06 21:40:08 +0530

    Add files via upload

---
+++ Flappy Bird/src/Renderer.java
@@ -0,0 +1,15 @@
+import java.awt.Graphics;
+import javax.swing.*;
+
+public class Renderer extends JPanel
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
